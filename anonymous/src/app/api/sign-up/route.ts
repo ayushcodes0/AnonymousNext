ac5970bab1bcccb1c2cfd22e1bebebd4ff25eb2e
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-06-04 23:27:08 +0530

    Started working on routing inside api folder

--- anonymous/src/app/api/sign-up/route.ts
+++ anonymous/src/app/api/sign-up/route.ts
@@ -1,3 +1,37 @@
 import dbConnect from "@/lib/dbConnect";
 import  UserModel  from "@/model/user";
 import bcrypt from "bcryptjs";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+
+export async function POST(request: Request){
+    await dbConnect();
+    try {
+
+        const {username, email, password} = await request.json();
+        const exitingUserVerifiedByUsername = await UserModel.findOne({
+            username: username,
+            isVerified: true
+        })
+
+        if(exitingUserVerifiedByUsername){
+            return Response.json({
+                success: false,
+                message: "Username already taken"
+            },{
+                status: 400
+            })
+        }
+
+        
+        
+    } catch (error) {
+        console.log("Error Registering User", error);
+        return Response.json({
+            success: false,
+            message: "Error Registering User"
+        },{
+            status: 500
+        })
+    }
+}
